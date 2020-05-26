6253a23086f270bb343abd378f2f7e40484fc023
Chaz L <chaz6cl@live.com>
2020-05-26 21:49:35 +1000

    Updated xss-payload.js again

--- xss-payload.js
+++ xss-payload.js
@@ -1,4 +1,2 @@
-<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-setTimeout(function(){$('body').removeAttr('style').html('<img src="https://image.ibb.co/byWcEv/rtaImage.jpg">').css('text-align','center').css('margin','125px');},2500);
 document.write ("This is a xss vuln");
 alert ("This is a xss vuln");
